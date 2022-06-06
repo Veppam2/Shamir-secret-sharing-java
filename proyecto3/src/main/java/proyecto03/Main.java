4785d270a3394fef760cf45b1acd4ef9f2f8534f
v23ec02 <v23ec02@ciencias.unam.mx>
2022-06-05 21:42:44 -0500

    FEAT: Mvn artifact

---
+++ proyecto3/src/main/java/proyecto03/Main.java
@@ -0,0 +1,12 @@
+package proyecto03;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
