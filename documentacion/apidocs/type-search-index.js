1c63c568a10524af0256aee36d203a5566eda8ab
v23ec02 <v23ec02@ciencias.unam.mx>
2022-06-14 20:48:42 -0500

    FEAT: Documentación en carpeta documentacion

---
+++ documentacion/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"proyecto03","l":"CifradorSecretoCompartido"},{"p":"proyecto03","l":"Main"},{"p":"proyecto03","l":"ManejadorArchivos"},{"p":"proyecto03","l":"Polinomio"}]
